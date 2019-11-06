8073e72a477022d1bcc9566dcbfc38cd3fcf4a6c
BradOdera <bradodera@gmail.com>
2019-11-06 16:33:48 +0300

    Add function to fix repos error

--- GitHub-Search/src/app/Components/profile/profile.component.ts
+++ GitHub-Search/src/app/Components/profile/profile.component.ts
@@ -8,7 +8,7 @@ import { RepositoryService } from "../../services/repository.service";
 })
 export class ProfileComponent implements OnInit {
   profile: any[];
-  repos: any[];
+  repos: any;
   // RepositoryService: typeof ProfileComponent;
   username: string;
   RepositoryService: Object;
@@ -19,11 +19,11 @@ export class ProfileComponent implements OnInit {
       this.RepositoryService = ProfileComponent;
     });
 
-    let repoUrl = this.RepositoryService['repos_url']
-    this.repositoryService.getProfileRepos(repoUrl).subscribe(_repos => {
-      console.log(this.repos);
-      this.repos = this.repos;
-    })
+    // let repoUrl = this.RepositoryService['repos_url']
+    // this.repositoryService.getProfileRepos(repoUrl).subscribe(_repos => {
+    //   console.log(this.repos);
+    //   this.repos = this.repos;
+    // })
   }
 
   pro: any;
@@ -37,10 +37,30 @@ export class ProfileComponent implements OnInit {
     this.RepositoryService = new Object(this.pro);
     console.log(this.RepositoryService);
 
-    });
+    let repoUrl = this.RepositoryService['repos_url']
+    this.repositoryService.getProfileRepos(repoUrl).subscribe(_repos => {
+      console.log(_repos);
+      this.repos = _repos;
+    })
+
+  });
 
   }
   ngOnInit() {
+    this.repositoryService.getProfileInfo().subscribe(data=>{
+      this.pro = data;  
+      console.log(this.pro);
+  
+      this.RepositoryService = new Object(this.pro);
+      console.log(this.RepositoryService);
+  
+      let repoUrl = this.RepositoryService['repos_url']
+      this.repositoryService.getProfileRepos(repoUrl).subscribe(_repos => {
+        console.log(_repos);
+        this.repos = _repos;
+      })
+  
+    });
   }
 
 }
